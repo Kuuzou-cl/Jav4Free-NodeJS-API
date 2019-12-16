01cb5543c64615119dd4ba5afb6c42add77fcdaf
devKuuzou <carlobernucci@gmail.com>
2019-12-16 15:57:11 -0300

    new category route fix

--- controllers/categories-controllers.js
+++ controllers/categories-controllers.js
@@ -75,8 +75,13 @@ const deleteCategory = async (req, res, next) => {
 }
 
 const getRandom4JavsCategory = async (req,res,next) => {
-    let categories = await Category.find({});
-    
+    let categories;
+    try {
+        categories = await Category.find({});
+    } catch (err) {
+        const error = new HttpError('Something went wrong', 500);
+        return next(error);
+    }
     res.json({ categories });
 }
 
