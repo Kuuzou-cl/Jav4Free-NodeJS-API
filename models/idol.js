e470b96fb0d6bed5363211fdcdf2301452158852
devKuuzou <carlobernucci@gmail.com>
2019-12-16 17:07:28 -0300

    new category route fix

--- models/idol.js
+++ models/idol.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const idolSchema = new Schema({
-    name:{ type:String, required: true },
+    name:{ type:String, required: true, unique: true  },
     imageUrl:{ type:String, required:true },
     hidden: { type: Boolean, default: false, required: true },
     creation: { type : Date, default: Date.now },
