af0edafffeb0320e4db1224f9221e71bc2e41a10
devKuuzou <carlobernucci@gmail.com>
2019-12-23 16:44:58 -0300

    route fix

--- controllers/categories-controllers.js
+++ controllers/categories-controllers.js
@@ -7,7 +7,7 @@ const Jav = require('../models/jav');
 const getCategories = async (req, res, next) => {
     let categories;
     try {
-        categories = await Category.find({}).sort({name:-1});
+        categories = await Category.find({}).sort({name: 'asc'});
     } catch (err) {
         const error = new HttpError('Something went wrong', 500);
         return next(error);
