e1c20a9075f2dd7943114ffe3d859f35e1779075
devKuuzou <carlobernucci@gmail.com>
2019-12-30 17:20:32 -0300

    fix

--- models/jav.js
+++ models/jav.js
@@ -3,16 +3,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const javSchema = new Schema({
-    name:{ type:String},
-    code:{ type:String},
-    url:{ type:String},
-    duration:{ type:String},
-    imageUrl:{ type:String},
-    imageIndexUrl:{ type:String},
-    hidden: { type: Boolean, default: false,},    
-    categories: [{type:mongoose.Types.ObjectId, ref:'jav4free-category'}],
-    idols: [{type:mongoose.Types.ObjectId, ref:'jav4free-idol'}],
-    creation: { type : Date, default: Date.now }
+    name: { type: String },
+    code: { type: String },
+    url: { type: String },
+    duration: { type: String },
+    imageUrl: { type: String },
+    imageIndexUrl: { type: String },
+    hidden: { type: Boolean, default: false, },
+    categories: { type: [mongoose.Types.ObjectId], ref: 'jav4free-category' },
+    idols: [{ type: mongoose.Types.ObjectId, ref: 'jav4free-idol' }],
+    creation: { type: Date, default: Date.now }
 })
 
-module.exports = mongoose.model('jav4free-jav',javSchema);
+module.exports = mongoose.model('jav4free-jav', javSchema);
