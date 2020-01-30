40a0f5edd9cd429af03a9f94cd7a38e057b05c87
devKuuzou <carlobernucci@gmail.com>
2020-01-29 23:35:27 -0300

    rollback

--- routes/upload-routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-const uploadControllers = require('../controllers/upload-controllers');
-
-router.post('/file', uploadControllers.index);
-
-module.exports = router;
