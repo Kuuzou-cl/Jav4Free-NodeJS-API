e036438faa3f20a94b0c8fe82b49f9c2c9661aca
devKuuzou <carlobernucci@gmail.com>
2020-01-30 21:53:58 -0300

    uploader function

--- routes/uploads-routes.js
+++ routes/uploads-routes.js
@@ -4,6 +4,8 @@ var router = express.Router();
 var multer = require('multer')
 var multerS3 = require('multer-s3')
 
+const HttpError = require('../models/http-error');
+
 const spacesEndpoint = new aws.Endpoint('sfo2.digitaloceanspaces.com');
 const s3 = new aws.S3({
     endpoint: spacesEndpoint,
@@ -12,23 +14,27 @@ const s3 = new aws.S3({
 });
 
 var upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: 'javdata',
-    acl: 'public-read',
-    metadata: function (req, file, cb) {
-      cb(null, {fieldName: file.fieldname});
-    },
-    key: function (req, file, cb) {
-      cb(null, Date.now()+file.originalname)
-    }
-  })
+    storage: multerS3({
+        s3: s3,
+        bucket: 'javdata/test',
+        acl: 'public-read',
+        metadata: function (req, file, cb) {
+            cb(null, { fieldName: file.fieldname });
+        },
+        key: function (req, file, cb) {
+            cb(null,file.originalname)
+        }
+    })
 })
 
 
-router.post('/upfile', upload.single('file'), function(req, res, next){
-  res.send(req.file);
-  console.log(req.file);
+router.post('/upJav', upload.single('file'), function (req, res, next) {
+    try {
+        res.send(req.file);
+        console.log(req.file);
+    } catch (err) {
+        res.status(201).json({ error: err })
+    }
 })
 
 
