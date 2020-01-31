8732bc79a239456e0b343c824f3c1ddef6d60f1f
devKuuzou <carlobernucci@gmail.com>
2020-01-30 21:19:32 -0300

    uploader function

--- app.js
+++ app.js
@@ -7,10 +7,33 @@ const categoriesRoutes = require('./routes/categories-routes');
 const userRoutes = require('./routes/users-routes');
 const idolsRoutes = require('./routes/idols-routes');
 const javsRoutes = require('./routes/javs-routes');
-const uploaderRoutes = require('./routes/uploader-routes');
 const HttpError = require('./models/http-error');
 
+const aws = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+
 const app = express();
+
+const spacesEndpoint = new aws.Endpoint('sfo2.digitaloceanspaces.com');
+const s3 = new aws.S3({
+    endpoint: spacesEndpoint,
+    accessKeyId: 'DMJFPZFGRYNJJIBZJHLZ',
+    secretAccessKey: '4LMidjQP20kuLkwxXDczG7G/TRb+xKH3yaDKbaqiKLY'
+});
+
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'javdata',
+        acl: 'public-read',
+        key: function (request, file, cb) {
+            console.log(file);
+            cb(null, file.originalname);
+        }
+    })
+}).array('upload', 1);
+
 
 app.use(cors())
 
@@ -24,7 +47,16 @@ app.use('/jav4free/idols', idolsRoutes);
 
 app.use('/jav4free/javs', javsRoutes);
 
-app.use('/jav4free/uploader', uploaderRoutes);
+app.post('/jav4free/upload', function (request, response, next) {
+    upload(request, response, function (error) {
+      if (error) {
+        console.log(error);
+        return response.redirect("/error");
+      }
+      console.log('File uploaded successfully.');
+      response.redirect("/success");
+    });
+  });
 
 app.use((req, res, next) => {
     const error = new HttpError('Could not find this route.', 404);
--- routes/uploader-routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const uploaderControllers = require('../controllers/uploader-controllers');
-
-const router = express.Router();
-
-router.post('/uploadFile',uploaderControllers.index);
-
-module.exports = router;
