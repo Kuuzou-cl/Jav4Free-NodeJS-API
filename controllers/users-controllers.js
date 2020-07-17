a0929f497641d52bee20d07a3e07e08cdf5920b2
Kuuzou-cl <carlobernucci@gmail.com>
2020-07-17 11:34:16 -0400

    update

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -49,7 +49,7 @@ const login = async (req, res, next) => {
         return next(error);
     } else {
 
-        res.json({ user: existingUser.email, userState: existingUser.admin, token: token })
+        res.json({ user: existingUser.email, userState: existingUser.admin })
     }
 };
 
