a943dd977bd14f5d12ebe0b507909403222fed8c
Kuuzou-cl <carlobernucci@gmail.com>
2020-07-17 10:15:39 -0400

    update

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,7 +1,17 @@
+const express = require('express');
 const auth = require("../middleware/auth");
-const bcrypt = require("bcrypt");
-const { User, validate } = require("../models/user");
-const express = require("express");
+const usersControllers = require('../controllers/users-controllers')
+
 const router = express.Router();
+
+// router.get('/', usersControllers.getUsers);
+
+router.post('/signup', usersControllers.signup);
+
+router.post('/login', usersControllers.login);
+
+// router.patch('/update/:uid', usersControllers.updateUser);
+
+// router.delete('/:uid', usersControllers.deleteUser);
 
 module.exports = router;
