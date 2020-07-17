ca7e80e4d0b340d2f1cbf8a1bdeacefc048b698c
Kuuzou-cl <carlobernucci@gmail.com>
2020-07-17 10:38:24 -0400

    update

--- models/user.js
+++ models/user.js
@@ -10,9 +10,4 @@ const UserSchema = new Schema({
     salt: String,
 })
 
-UsersSchema.methods.setPassword = function (password) {
-    this.salt = crypto.randomBytes(16).toString('hex');
-    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
-};
-
 module.exports = mongoose.model('User', UserSchema);
