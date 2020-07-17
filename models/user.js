d8dfdc39cea420c93ce459b976b88a80728220db
Kuuzou-cl <carlobernucci@gmail.com>
2020-07-17 09:57:09 -0400

    update

--- models/user.js
+++ models/user.js
@@ -1,3 +1,6 @@
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
@@ -25,4 +28,17 @@ const userSchema = new Schema({
     isAdmin: Boolean
 })
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user) {
+    const schema = {
+        name: Joi.string().min(3).max(50).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(3).max(255).required()
+    };
+
+    return Joi.validate(user, schema);
+}
+
+exports.User = User; 
+exports.validate = validateUser;
