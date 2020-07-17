ff86272c968ce431d6ea0721bef716bfe24515ed
Kuuzou-cl <carlobernucci@gmail.com>
2020-07-17 10:03:51 -0400

    update

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -4,9 +4,5 @@ const usersControllers = require('../controllers/users-controllers')
 
 const router = express.Router();
 
-router.get("/current", auth, async (req, res) => {
-    const user = await User.findById(req.user._id).select("-password");
-    res.send(user);
-});
 
 module.exports = router;
