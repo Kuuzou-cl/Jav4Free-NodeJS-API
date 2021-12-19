8dfec055257c775151a4e78fc5ebfb92fd8940fd
Carlo Bernucci Gómez <carlobernucci@gmail.com>
2021-12-19 14:57:23 -0300

    search videos r34h

--- controllers/videos-controllers.js
+++ controllers/videos-controllers.js
@@ -184,12 +184,21 @@ const searchVideos = async (req, res, next) => {
             const error = new HttpError('Error searching for category.', 404);;
             return next(error);
         } else {
-            categoriesMatch.push(category.id);
+            if (!categoriesMatch.some(item => item._id === category._id)) {
+                categoriesMatch.push(category);
+            }
         }
     }
 
     let results = [];
 
+    for (const video of videos) {
+        for (const category of categoriesMatch) {
+            if (video.categories.some(item => item === category._id)) {
+                results.push(video);
+            }
+        }
+    }
 
     
     let nextPage;
